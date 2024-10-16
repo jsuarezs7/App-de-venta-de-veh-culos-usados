550f0548380c2bab92ce8c93ea2c6dca769ac295
Juan David Linares <jlinaresc3@ucentral.edu.co>
2024-10-15 19:50:42 -0500

    (Nuevo:Estructura)

--- vehiculos_usados/src/main/java/co/edu/ucentral/paginavehiculos/vehiculos_usados/Persistencia/Repositorios/VehiculosRepositorio.java
+++ vehiculos_usados/src/main/java/co/edu/ucentral/paginavehiculos/vehiculos_usados/Persistencia/Repositorios/VehiculosRepositorio.java
@@ -1,2 +1,4 @@
-package co.edu.ucentral.paginavehiculos.vehiculos_usados.Persistencia.Repositorios;public interface VehiculosRepositorio {
+package co.edu.ucentral.paginavehiculos.vehiculos_usados.Persistencia.Repositorios;
+
+public interface VehiculosRepositorio {
 }
--- vehiculos_usados/src/main/java/co/edu/ucentral/paginavehiculos/vehiculos_usados/Servicios/VehiculosServicio.java
+++ vehiculos_usados/src/main/java/co/edu/ucentral/paginavehiculos/vehiculos_usados/Servicios/VehiculosServicio.java
@@ -1,2 +1,4 @@
-package co.edu.ucentral.paginavehiculos.vehiculos_usados.Servicios;public class VehiculosServicio {
+package co.edu.ucentral.paginavehiculos.vehiculos_usados.Servicios;
+
+public class VehiculosServicio {
 }
